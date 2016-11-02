5f3dd144643d7d418938fc1c24e637b1fb070aab
Martin Fencl <martin.fencl@gmail.com>
2016-11-02 15:56:15 -0500

    Edit and Delete Posts.'

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,7 +11,7 @@ class PostsController < ApplicationController
     @post = Post.new( post_params )
 
     if @post.save
-      redirect_to @post, notice: 'Success!'
+      redirect_to @post, notice: 'Successfully created!'
     else
       render 'new'
     end
@@ -20,6 +20,27 @@ class PostsController < ApplicationController
   def show
     @post = Post.find( params[:id] )
   end
+
+  def edit
+    @post = Post.find( params[:id] )
+  end
+
+  def update
+    @post = Post.find( params[:id] )
+
+    if @post.update( post_params )
+      redirect_to @post, notice: "Successfully updated!"
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @post = Post.find( params[:id] )
+    @post.destroy
+
+    redirect_to root_path, notice: 'Successfully deleted!'
+   end
 
   private
   def post_params
